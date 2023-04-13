a7aa8427c0d0bd24d2bf622a2d2cd2322d4496c3
ellis <lsp1@hotmail.com>
2023-04-13 16:46:52 -0600

    add '.' to image paths useWYWMImages

--- src/hooks/useWYWMImages.jsx
+++ src/hooks/useWYWMImages.jsx
@@ -13,31 +13,31 @@ function useWYWMImages() {
       alt: "Intermediate web dev",
     },
     {
-      src: "src/images/WYWM-certificates/WYWM-BigO.JPG",
+      src: "./src/images/WYWM-certificates/WYWM-BigO.JPG",
       alt: "Big-O",
     },
     {
-      src: "src/images/WYWM-certificates/WYWM-DevOps.JPG",
+      src: "./src/images/WYWM-certificates/WYWM-DevOps.JPG",
       alt: "DevOps",
     },
     {
-      src: "src/images/WYWM-certificates/WYWM-IntroWebDev.JPG",
+      src: "./src/images/WYWM-certificates/WYWM-IntroWebDev.JPG",
       alt: "Intro WebDev",
     },
     {
-      src: "src/images/WYWM-certificates/WYWM-SourceControlFundamentals.jpg",
+      src: "./src/images/WYWM-certificates/WYWM-SourceControlFundamentals.jpg",
       alt: "Source Control",
     },
     {
-      src: "src/images/WYWM-certificates/WYWM-PrinciplesOfProgramming.JPG",
+      src: "./src/images/WYWM-certificates/WYWM-PrinciplesOfProgramming.JPG",
       alt: "Principles Of Programming",
     },
     {
-      src: "src/images/WYWM-certificates/WYWM-SoftwareQATestingFundamentals.JPG",
+      src: "./src/images/WYWM-certificates/WYWM-SoftwareQATestingFundamentals.JPG",
       alt: "QA Testing",
     },
     {
-      src: "src/images/WYWM-certificates/WYWM-IntroductionToDigitalProjectManagement.jpg",
+      src: "./src/images/WYWM-certificates/WYWM-IntroductionToDigitalProjectManagement.jpg",
       alt: "Digital Project Management",
     },
   ];
