82efe653706f87bf9dc43f21900a48e1174bde5b
ellis <lsp1@hotmail.com>
2023-04-14 12:33:04 -0600

    create hamburger component

---
+++ src/components/Nav/nav-svgs/Hamburger.jsx
@@ -0,0 +1,21 @@
+function Hamburger({ expandMenu }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-12 h-12 text-info-75 sm:hidden absolute ml-3 cursor-pointer hover:text-info-50"
+      onClick={expandMenu}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
+}
+
+export default Hamburger;
