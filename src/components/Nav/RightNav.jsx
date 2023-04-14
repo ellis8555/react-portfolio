25a816b1cca77b05689a51b6c11e51ed441326ca
ellis <lsp1@hotmail.com>
2023-04-14 12:32:50 -0600

    add paddings to menu items xs-screen

--- src/components/Nav/RightNav.jsx
+++ src/components/Nav/RightNav.jsx
@@ -1,21 +1,26 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
-function RightNav({links}){
-  return(
+function RightNav({ links }) {
+  return (
     <ul className="flex flex-col sm:gap-x-2 sm:flex-row sm:items-center">
-    {links.map((link, index) => {
-    return link.float ? (
-        <NavLink className={"rounded-md w-full sm:hover:bg-black-75 " + (link.color ? " " + link.color : "")}
-                 key={index} 
-                 to={link.linkName}>
-                    <li className="hover:bg-black-75 sm:p-4">
-                      {link.componentName}
-                    </li>
-                  </NavLink>
-    ) : null;
-  })}
-      </ul>
-)
+      {links.map((link, index) => {
+        return link.float ? (
+          <NavLink
+            className={
+              "rounded-md w-full sm:hover:bg-black-75 " +
+              (link.color ? " " + link.color : "")
+            }
+            key={index}
+            to={link.linkName}
+          >
+            <li className="hover:bg-black-75 py-2 pl-2 sm:p-4">
+              {link.componentName}
+            </li>
+          </NavLink>
+        ) : null;
+      })}
+    </ul>
+  );
 }
 
-export default RightNav
+export default RightNav;
