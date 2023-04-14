483062440e10c8261596f05624878a4cf0e33b6a
ellis <lsp1@hotmail.com>
2023-04-13 20:42:07 -0600

    import correction

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,32 +2,29 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
-} from "react-router-dom"
-import RootLayout from "../../Layouts/RootLayout"
-import Home from "../Home/home"
-import Contact from "../Contact/Contact"
-import Login from "../Login/Login"
-import Logout from "../Logout/Logout"
-import SignUp from "../SignUp/SignUp"
-
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "../../Layouts/RootLayout";
+import Home from "../Home/Home";
+import Contact from "../Contact/Contact";
+import Login from "../Login/Login";
+import Logout from "../Logout/Logout";
+import SignUp from "../SignUp/SignUp";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />}/>
-      <Route path="contact" element={<Contact />}/>
-      <Route path="login" element={<Login />}/>
-      <Route path="logout" element={<Logout />}/>
-      <Route path="signUp" element={<SignUp />}/>
+      <Route index element={<Home />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="login" element={<Login />} />
+      <Route path="logout" element={<Logout />} />
+      <Route path="signUp" element={<SignUp />} />
     </Route>
   )
-)
+);
 
 function App() {
-  return (
-      <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
