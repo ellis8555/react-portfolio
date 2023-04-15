83c52eb1b3bb7af4794a88759f31c9bbbefad6d5
ellis <lsp1@hotmail.com>
2023-04-15 08:46:10 -0600

    remove transitions on certs slide methods

--- src/components/Certificates/Certificate-methods/slideLeft.js
+++ src/components/Certificates/Certificate-methods/slideLeft.js
@@ -1,15 +1,11 @@
-function slideLeft({ image, imageCount, currentIndex, setImageIndex }) {
-  image.current.classList.add("opacity-10");
-  setTimeout(() => {
-    image.current.classList.remove("opacity-10");
-    if (currentIndex.current > 0) {
-      setImageIndex((prevIndex) => prevIndex - 1);
-      currentIndex.current = currentIndex.current - 1;
-    } else {
-      setImageIndex(imageCount);
-      currentIndex.current = imageCount;
-    }
-  }, 300);
+function slideLeft({ imageCount, currentIndex, setImageIndex }) {
+  if (currentIndex.current > 0) {
+    setImageIndex((prevIndex) => prevIndex - 1);
+    currentIndex.current = currentIndex.current - 1;
+  } else {
+    setImageIndex(imageCount);
+    currentIndex.current = imageCount;
+  }
 }
 
 export default slideLeft;
--- src/components/Certificates/Certificate-methods/slideRight.js
+++ src/components/Certificates/Certificate-methods/slideRight.js
@@ -1,15 +1,11 @@
-function slideRight({ image, imageCount, currentIndex, setImageIndex }) {
-  image.current.classList.add("opacity-30");
-  setTimeout(() => {
-    image.current.classList.remove("opacity-30");
-    if (currentIndex.current < imageCount) {
-      setImageIndex((prevIndex) => prevIndex + 1);
-      currentIndex.current = currentIndex.current + 1;
-    } else {
-      setImageIndex(0);
-      currentIndex.current = 0;
-    }
-  }, 300);
+function slideRight({ imageCount, currentIndex, setImageIndex }) {
+  if (currentIndex.current < imageCount) {
+    setImageIndex((prevIndex) => prevIndex + 1);
+    currentIndex.current = currentIndex.current + 1;
+  } else {
+    setImageIndex(0);
+    currentIndex.current = 0;
+  }
 }
 
 export default slideRight;
