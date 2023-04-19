51dec0844c3bb11c250b1ddfa43461c23b6b573b
ellis <lsp1@hotmail.com>
2023-04-19 11:07:59 -0600

    create setSuccessColor.js

---
+++ src/utils/util-methods/setSuccessColor.js
@@ -0,0 +1,28 @@
+const setSuccessColors = (
+  inputElement,
+  setOutlineColor,
+  helpElement,
+  setHelpColor,
+  ...removeColorList
+) => {
+  if (helpElement.current.classList.contains(setHelpColor)) return;
+
+  for (let i = 0; i <= removeColorList.length; i++) {
+    if (helpElement.current.classList.contains(removeColorList[i])) {
+      helpElement.current.classList.remove(removeColorList[i]);
+      break;
+    }
+  }
+
+  for (let i = 0; i <= removeColorList.length; i++) {
+    if (inputElement.current.classList.contains(removeColorList[i])) {
+      inputElement.current.classList.remove(removeColorList[i]);
+      break;
+    }
+  }
+
+  inputElement.current.classList.add("outline", "outline-2", setOutlineColor);
+  helpElement.current.classList.add(setHelpColor);
+};
+
+export { setSuccessColors };
