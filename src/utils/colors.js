900b1d90630324cd65324287431754cb441750a9
ellis <lsp1@hotmail.com>
2023-04-19 16:33:54 -0600

    rename slate-500 to unselected in colors.js

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,10 +1,10 @@
 const colors = {
   TEXT_SUCCESS: "text-green-600",
   TEXT_DANGER: "text-danger",
-  TEXT_SLATE_500: "text-slate-500",
+  TEXT_UNSELECTED: "text-slate-500",
   OUTLINE_SUCCESS: "outline-green-500",
   OUTLINE_DANGER: "outline-danger",
-  OUTLINE_SLATE_500: "outline-slate-500",
+  OUTLINE_UNSELECTED: "outline-slate-500",
 };
 
 export default colors;
