76f36dd02830c9cfaac4bb7138c3fc5d80f6e77d
ellis <lsp1@hotmail.com>
2023-04-19 11:12:15 -0600

    create colors.js

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  TEXT_SUCCESS: "text-green-600",
+  TEXT_DANGER: "text-danger",
+  TEXT_SLATE_500: "text-slate-500",
+  OUTLINE_SUCCESS: "outline-green-500",
+  OUTLINE_DANGER: "outline-danger",
+  OUTLINE_SLATE_500: "outline-slate-500",
+};
+
+export default colors;
