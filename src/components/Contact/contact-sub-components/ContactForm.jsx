70e3be327f6badbdf7548c8fb68424f9dcc58151
ellis <lsp1@hotmail.com>
2023-04-19 11:09:43 -0600

    ContactForm.jsx break down into components

--- src/components/Contact/contact-sub-components/ContactForm.jsx
+++ src/components/Contact/contact-sub-components/ContactForm.jsx
@@ -1,16 +1,31 @@
-import NameInput from "./contact-form/NameInput";
-import CommentInput from "./contact-form/CommentInput";
+import { useState } from "react";
+import NameInput from "./contact-form-sub-components/NameInput";
+import CommentInput from "./contact-form-sub-components/CommentInput";
 
 function ContactForm() {
+  const [userName, setUserName] = useState("");
+
+  const updateUserName = (newName) => {
+    setUserName(newName);
+  };
+
+  const submitContactForm = (e) => {
+    e.preventDefault();
+    // console.log("inside ContactForm");
+  };
+
   return (
-    <form className="w-full m-3 my-8 sm:mx-10 lg:w-3/4" name="contactForm">
-      <NameInput />
+    <form
+      className="w-full m-3 my-8 sm:mx-10 lg:w-3/4"
+      name="contactForm"
+      onSubmit={submitContactForm}
+    >
+      <NameInput getUserName={userName} updateUserName={updateUserName} />
       <CommentInput />
       <button
         type="submit"
         className="bg-blue-600 rounded-md mt-4 px-3 py-2"
         name="submit"
-        disabled
       >
         Submit
       </button>
