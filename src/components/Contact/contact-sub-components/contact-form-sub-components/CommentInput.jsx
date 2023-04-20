70f95dee4dadc57a389a9a5cba9acc8407c92173
ellis <lsp1@hotmail.com>
2023-04-19 22:37:25 -0600

    CommentInput.jsx added input validation

--- src/components/Contact/contact-sub-components/contact-form-sub-components/CommentInput.jsx
+++ src/components/Contact/contact-sub-components/contact-form-sub-components/CommentInput.jsx
@@ -1,20 +1,92 @@
-function CommentInput() {
+import { useState, useEffect, useRef } from "react";
+import { userCommentValidator } from "../../../../utils/user-input/validation/validation-tests/contact-comment-input";
+import { setSuccessColors } from "../../../../utils/util-methods/setSuccessColor";
+import onBlurZeroInput from "../../../../utils/util-methods/onBlurZeroInput";
+import colors from "../../../../utils/colors";
+import CheckSvg from "./contact-form-svgs/CheckSvg";
+
+function CommentInput({ getUserComment, updateUserComment }) {
+  const [valid, setValid] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(true);
+  const commentInputRef = useRef();
+  const commentHelpRef = useRef();
+
+  const userInputRefs = {
+    inputElement: commentInputRef,
+    helpElement: commentHelpRef,
+    getUserComment: getUserComment,
+  };
+
+  useEffect(() => {
+    if (userInputRefs.getUserComment.length > 0) {
+      setIsEmpty(false);
+      const isValid = userCommentValidator(userInputRefs.getUserComment);
+      if (isValid) {
+        setValid(true);
+        setSuccessColors(
+          userInputRefs.inputElement,
+          colors.OUTLINE_SUCCESS,
+          userInputRefs.helpElement,
+          colors.TEXT_SUCCESS,
+          ...Object.values(colors)
+        );
+      } else {
+        setValid(false);
+        setSuccessColors(
+          userInputRefs.inputElement,
+          colors.OUTLINE_DANGER,
+          userInputRefs.helpElement,
+          colors.TEXT_DANGER,
+          ...Object.values(colors)
+        );
+      }
+    } else {
+      setValid(false);
+      setIsEmpty(true);
+      setSuccessColors(
+        userInputRefs.inputElement,
+        colors.OUTLINE_UNSELECTED,
+        userInputRefs.helpElement,
+        colors.TEXT_UNSELECTED,
+        ...Object.values(colors)
+      );
+    }
+  }, [userInputRefs.getUserComment]);
+
   return (
     <>
       <div className="mt-8 flex flex-col gap-2">
         <label className="text-info" htmlFor="comment">
-          Comment:
+          Comment: {!isEmpty && `${getUserComment.length} /250`}
         </label>
         <div className="w-full">
           <textarea
             id="comment"
-            className="rounded-md p-2 text-black text-lg w-full xl: w-3/4 focus:outline outline-2"
+            ref={userInputRefs.inputElement}
+            className="rounded-md p-2 text-black text-lg w-full xl: w-3/4"
             name="comment"
+            onChange={(e) => {
+              updateUserComment(e.target.value);
+            }}
+            onBlur={() => {
+              onBlurZeroInput(
+                userInputRefs.inputElement,
+                userInputRefs.getUserComment,
+                "outline-0",
+                "outline",
+                "outline-2",
+                colors.TEXT_UNSELECTED
+              );
+            }}
             placeholder="Drop me a comment or leave some feedback"
             rows="10"
           ></textarea>
-          <div className="text-slate-500 text-sm" id="commentHelp">
-            <p>Between 8 - 250 characters</p>
+          <div
+            id="commentHelp"
+            ref={userInputRefs.helpElement}
+            className="text-slate-500 text-sm"
+          >
+            {valid ? <CheckSvg /> : <p>Between 8 - 250 characters</p>}
           </div>
         </div>
       </div>
