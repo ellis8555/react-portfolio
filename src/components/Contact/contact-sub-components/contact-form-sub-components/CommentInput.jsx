0612f7f9aa28b41516eaff1c978ed7c7757c338d
ellis <lsp1@hotmail.com>
2023-04-20 08:05:30 -0600

    CheckSvg and input count added to CommentInput.jsx

--- src/components/Contact/contact-sub-components/contact-form-sub-components/CommentInput.jsx
+++ src/components/Contact/contact-sub-components/contact-form-sub-components/CommentInput.jsx
@@ -56,37 +56,50 @@ function CommentInput({ getUserComment, updateUserComment }) {
   return (
     <>
       <div className="mt-8 flex flex-col gap-2">
-        <label className="text-info" htmlFor="comment">
-          Comment: {!isEmpty && `${getUserComment.length} /250`}
-        </label>
+        <div>
+          <label className="text-info" htmlFor="comment">
+            Comment:
+          </label>
+          {!isEmpty ? (
+            <span className={"ml-2" + " " + (!valid && colors.TEXT_DANGER)}>
+              {getUserComment.length}
+              <span className="text-info"> /250</span>
+            </span>
+          ) : (
+            ""
+          )}
+        </div>
         <div className="w-full">
-          <textarea
-            id="comment"
-            ref={userInputRefs.inputElement}
-            className="rounded-md p-2 text-black text-lg w-full xl: w-3/4"
-            name="comment"
-            onChange={(e) => {
-              updateUserComment(e.target.value);
-            }}
-            onBlur={() => {
-              onBlurZeroInput(
-                userInputRefs.inputElement,
-                userInputRefs.getUserComment,
-                "outline-0",
-                "outline",
-                "outline-2",
-                colors.TEXT_UNSELECTED
-              );
-            }}
-            placeholder="Drop me a comment or leave some feedback"
-            rows="10"
-          ></textarea>
+          <div className="relative">
+            <textarea
+              id="comment"
+              ref={userInputRefs.inputElement}
+              className="rounded-md p-2 text-black text-lg w-full xl: w-3/4"
+              name="comment"
+              onChange={(e) => {
+                updateUserComment(e.target.value);
+              }}
+              onBlur={() => {
+                onBlurZeroInput(
+                  userInputRefs.inputElement,
+                  userInputRefs.getUserComment,
+                  "outline-0",
+                  "outline",
+                  "outline-2",
+                  colors.TEXT_UNSELECTED
+                );
+              }}
+              placeholder="Drop me a comment or leave some feedback"
+              rows="10"
+            ></textarea>
+            {valid && <CheckSvg classes="absolute top-2 right-2" />}
+          </div>
           <div
             id="commentHelp"
             ref={userInputRefs.helpElement}
             className="text-slate-500 text-sm"
           >
-            {valid ? <CheckSvg /> : <p>Between 8 - 250 characters</p>}
+            <p>Between 8 - 250 characters</p>
           </div>
         </div>
       </div>
