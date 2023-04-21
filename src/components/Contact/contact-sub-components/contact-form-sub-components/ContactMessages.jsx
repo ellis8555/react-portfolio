e1c910925587fc21ed2f5dd2e44c03fbd4951128
ellis <lsp1@hotmail.com>
2023-04-21 16:58:34 -0600

    create ContactMessages.jsx

---
+++ src/components/Contact/contact-sub-components/contact-form-sub-components/ContactMessages.jsx
@@ -0,0 +1,15 @@
+function ContactMessages() {
+  return (
+    <>
+      <p className="text-center h2 mt-10 line-through">
+        Leave a comment or feedback
+      </p>
+      <p className="text-center text-info h2">Form validation visuals set.</p>
+      <p className="text-center text-warning h4">
+        Form submission near complete
+      </p>
+    </>
+  );
+}
+
+export default ContactMessages;
