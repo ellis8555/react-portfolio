0f50f7fd74bbd4b05ab81c57219ad3319c8d9484
ellis <lsp1@hotmail.com>
2023-04-21 16:59:26 -0600

    add animations to Home.jsx upon form submit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,58 @@
+import { useRef, useEffect, useContext } from "react";
+import { ProvideDisplayAlert } from "../App/App";
 import BodyContainer from "../../utils/components/BodyContainer";
+import HomeAlertMessage from "./Home-components/HomeAlertMessage";
 import Construction from "../Construction/Construction";
 import HomeMessagePoints from "./Home-components/HomeMessagePoints";
 import Certificates from "../Certificates/Certificates";
 
 function Home() {
+  const { displayAlert, setDisplayAlert } = useContext(ProvideDisplayAlert);
+  const messageParagraph = useRef();
+
+  useEffect(() => {
+    if (displayAlert) {
+      document.documentElement.scrollIntoView(top);
+      let innerInterval;
+      const timeToHideDisplayAlert =
+        (parseInt(getComputedStyle(messageParagraph.current).height) / 10) *
+          10 +
+        3075;
+
+      const beginReductionOfHeight = setTimeout(() => {
+        innerInterval = setInterval(() => {
+          let messageParagraphHeight = parseInt(
+            getComputedStyle(messageParagraph.current).height
+          );
+          if (messageParagraphHeight > 0) {
+            messageParagraph.current.style.height = `${
+              messageParagraphHeight - 4
+            }px`;
+          }
+        }, 10);
+      }, 3000);
+
+      const showAlert = setTimeout(() => {
+        setDisplayAlert(false);
+      }, timeToHideDisplayAlert);
+      return () => {
+        clearTimeout(showAlert);
+        clearTimeout(beginReductionOfHeight);
+        clearInterval(innerInterval);
+      };
+    }
+  }, [displayAlert]);
+
   return (
     <>
+      {displayAlert && (
+        <BodyContainer>
+          <HomeAlertMessage
+            messageParagraph={messageParagraph}
+            displayAlert={displayAlert}
+          />
+        </BodyContainer>
+      )}
       <BodyContainer title="My React Portfolio">
         <Construction />
       </BodyContainer>
