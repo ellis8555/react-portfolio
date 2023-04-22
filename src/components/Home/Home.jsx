a0b309ef6fdb187d6d0aa03f91479b0071679e4f
ellis <lsp1@hotmail.com>
2023-04-22 09:18:57 -0600

    remove displayAlert useEffect from Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
-import { useRef, useEffect, useContext } from "react";
+import { useRef, useContext } from "react";
 import { ProvideDisplayAlert } from "../App/App";
+import useDisplayAlert from "../../hooks/useDisplayAlert";
 import BodyContainer from "../../utils/components/BodyContainer";
 import HomeAlertMessage from "./Home-components/HomeAlertMessage";
 import Construction from "../Construction/Construction";
@@ -10,49 +11,7 @@ function Home() {
   const { displayAlert, setDisplayAlert } = useContext(ProvideDisplayAlert);
   const messageParagraph = useRef();
 
-  useEffect(() => {
-    if (displayAlert) {
-      document.documentElement.scrollIntoView(top);
-      const minimizeTimings = {
-        reduceHeightBy: 4,
-        reduceHeightOnTheseSeconds: 10,
-        timeToBeginHidingAlert: 3000,
-        additionalBufferTime: 25,
-        timeToHideDisplayAlert: function () {
-          return (
-            (parseInt(getComputedStyle(messageParagraph.current).height) /
-              this.reduceHeightOnTheseSeconds) *
-              this.reduceHeightOnTheseSeconds +
-            this.timeToBeginHidingAlert +
-            this.additionalBufferTime
-          );
-        },
-      };
-      let innerInterval;
-
-      const beginReductionOfHeight = setTimeout(() => {
-        innerInterval = setInterval(() => {
-          let messageParagraphHeight = parseInt(
-            getComputedStyle(messageParagraph.current).height
-          );
-          if (messageParagraphHeight > 0) {
-            messageParagraph.current.style.height = `${
-              messageParagraphHeight - minimizeTimings.reduceHeightBy
-            }px`;
-          }
-        }, minimizeTimings.reduceHeightOnTheseSeconds);
-      }, minimizeTimings.timeToBeginHidingAlert);
-
-      const showAlert = setTimeout(() => {
-        setDisplayAlert(false);
-      }, minimizeTimings.timeToHideDisplayAlert());
-      return () => {
-        clearTimeout(showAlert);
-        clearTimeout(beginReductionOfHeight);
-        clearInterval(innerInterval);
-      };
-    }
-  }, [displayAlert]);
+  useDisplayAlert(displayAlert, setDisplayAlert, messageParagraph);
 
   return (
     <>
