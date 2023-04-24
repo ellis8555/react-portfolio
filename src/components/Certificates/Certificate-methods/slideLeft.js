688c18ac7ed601be7ff0e212384ce6ee5801b37c
ellis <lsp1@hotmail.com>
2023-04-24 10:36:42 -0600

    add scrollintoview to slideLeft/Right.js

--- src/components/Certificates/Certificate-methods/slideLeft.js
+++ src/components/Certificates/Certificate-methods/slideLeft.js
@@ -2,9 +2,13 @@ function slideLeft({ imageCount, currentIndex, setImageIndex }) {
   if (currentIndex.current > 0) {
     setImageIndex((prevIndex) => prevIndex - 1);
     currentIndex.current = currentIndex.current - 1;
+    const imgExample = document.getElementById(`img${currentIndex.current}`);
+    imgExample.scrollIntoView({ behavior: "smooth", inline: "center" });
   } else {
-    setImageIndex(imageCount);
-    currentIndex.current = imageCount;
+    setImageIndex(0);
+    currentIndex.current = 0;
+    const imgExample = document.getElementById(`img${currentIndex.current}`);
+    imgExample.scrollIntoView({ behavior: "smooth", inline: "center" });
   }
 }
 
--- src/components/Certificates/Certificate-methods/slideRight.js
+++ src/components/Certificates/Certificate-methods/slideRight.js
@@ -2,9 +2,13 @@ function slideRight({ imageCount, currentIndex, setImageIndex }) {
   if (currentIndex.current < imageCount) {
     setImageIndex((prevIndex) => prevIndex + 1);
     currentIndex.current = currentIndex.current + 1;
+    const imgExample = document.getElementById(`img${currentIndex.current}`);
+    imgExample.scrollIntoView({ behavior: "smooth", inline: "center" });
   } else {
-    setImageIndex(0);
-    currentIndex.current = 0;
+    setImageIndex(imageCount);
+    currentIndex.current = imageCount;
+    const imgExample = document.getElementById(`img${currentIndex.current}`);
+    imgExample.scrollIntoView({ behavior: "smooth", inline: "center" });
   }
 }
 
