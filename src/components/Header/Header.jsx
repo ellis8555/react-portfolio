13e22490f56ce4e8fe00aff8be1a673dd7efc114
ellis <lsp1@hotmail.com>
2023-04-26 09:38:10 -0600

    Header.jsx remove setNavBackground method

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,17 +4,17 @@ import LeftNav from "../Nav/LeftNav";
 import RightNav from "../Nav/RightNav";
 import Hamburger from "../Nav/nav-svgs/Hamburger";
 import expandMenu from "../Nav/nav-methods/expandMenu";
-import setNavBackground from "../Nav/nav-methods/setNavBackground";
 
 function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [yPos, setYPos] = useState(window.pageYOffset);
   const header = useRef();
   const nav = useRef();
   const links = useNavLinks(isLoggedIn);
 
   useEffect(() => {
     const setBgOnScroll = () => {
-      setNavBackground(nav);
+      setYPos(window.pageYOffset);
     };
 
     window.addEventListener("scroll", setBgOnScroll);
@@ -38,7 +38,11 @@ function Header() {
         >
           <nav
             ref={nav}
-            className="overflow-hidden bg-primary-25 w-full flex flex-col sm:h-full sm:rounded-md sm:flex-row sm:justify-between"
+            className={
+              "overflow-hidden w-full flex flex-col sm:h-full sm:rounded-md sm:flex-row sm:justify-between" +
+              " " +
+              (yPos < 69 ? "bg-primary-25" : "bg-info-90")
+            }
           >
             <LeftNav links={links} />
             <RightNav links={links} />
