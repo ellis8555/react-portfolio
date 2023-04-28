e6e199cb6940abf9b7e8ecbb8dbe3ea77eea82e3
ellis <lsp1@hotmail.com>
2023-04-28 16:15:15 -0600

    add okToHideNavBar to displayalertcontext.jsx

--- src/contexts/DisplayAlertContext.jsx
+++ src/contexts/DisplayAlertContext.jsx
@@ -1,15 +1,18 @@
-import { useState, createContext } from "react";
+import { useState, useRef, createContext } from "react";
 
 const DisplayAlertContext = createContext();
 
 function ProvideDisplayAlert({ children }) {
   const [displayAlert, setDisplayAlert] = useState(false);
   const [messageToDisplay, setMessageToDisplay] = useState("");
+  const okToHideNavBar = useRef(false);
+
   const displayValues = {
     displayAlert,
     setDisplayAlert,
     messageToDisplay,
     setMessageToDisplay,
+    okToHideNavBar,
   };
 
   return (
