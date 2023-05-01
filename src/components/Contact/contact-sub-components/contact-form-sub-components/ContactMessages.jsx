fee1e20302696dd526819de64bd3355ed669825e
ellis <lsp1@hotmail.com>
2023-05-01 15:19:09 -0600

    contact form message underconstruction removed

--- src/components/Contact/contact-sub-components/contact-form-sub-components/ContactMessages.jsx
+++ src/components/Contact/contact-sub-components/contact-form-sub-components/ContactMessages.jsx
@@ -1,13 +1,7 @@
 function ContactMessages() {
   return (
     <>
-      <p className="text-center h2 mt-10 line-through">
-        Leave a comment or feedback
-      </p>
-      <p className="text-center text-info h2">Form validation visuals set.</p>
-      <p className="text-center text-warning h4">
-        Form submission near complete
-      </p>
+      <p className="text-center h2 mt-10">Leave a comment or feedback</p>
     </>
   );
 }
