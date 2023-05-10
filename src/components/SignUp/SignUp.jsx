a8e6903ecf47b8cae82338806ea92fa727f4bf49
ellis <lsp1@hotmail.com>
2023-05-10 12:21:55 -0600

    signup.jsx now displays functioning form

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+import LoadingAnimation from "../../utils/components/LoadingAnimation";
+import SignupForm from "./signup-form-sub-components/SignupForm";
+import SignupMessages from "./signup-form-sub-components/SignupMessages";
 import BodyContainer from "../../utils/components/BodyContainer";
 
-function SignUpContent() {
-  return <p className="h4 text-warning">Under construction</p>;
-}
-
 function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <BodyContainer title="Sign up">
-      <SignUpContent />
-    </BodyContainer>
+    <>
+      <div className=" lg:flex lg:justify-center">
+        <BodyContainer title="Sign up" width="lg:w-3/4">
+          <SignupMessages />
+        </BodyContainer>
+      </div>
+      {isLoading && <LoadingAnimation />}
+      <div className=" md:flex md:justify-center">
+        <BodyContainer width="md:w-3/4 lg:w-1/2">
+          <SignupForm setIsLoading={setIsLoading} />
+        </BodyContainer>
+      </div>
+    </>
   );
 }
 
