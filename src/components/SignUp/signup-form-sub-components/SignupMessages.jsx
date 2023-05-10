02403a6926782969606f7ce9056d69ef8804db06
ellis <lsp1@hotmail.com>
2023-05-10 12:21:17 -0600

    signUpMessage created

---
+++ src/components/SignUp/signup-form-sub-components/SignupMessages.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SignupMessages() {
+  return (
+    <p className="h5 p-2">
+      Account creation is solely for demonstration purposes. An account is
+      actually created in which the user can login and out.
+    </p>
+  );
+}
+
+export default SignupMessages;
