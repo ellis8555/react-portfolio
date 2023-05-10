5695b1b478cbdfeb4fbb57479719e8711d14ee08
ellis <lsp1@hotmail.com>
2023-05-10 12:24:08 -0600

    logout.jsx functions

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,7 +1,25 @@
-function Logout(){
-    return(
-        <div>Logout page!</div>
-    )
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { DisplayAlertContext } from "../../contexts/DisplayAlertContext";
+
+function Logout() {
+  const navigate = useNavigate();
+  const {
+    setIsLoggedIn,
+    loggedInUser,
+    setMessageToDisplay,
+    okToHideNavBar,
+    setDisplayAlert,
+  } = useContext(DisplayAlertContext);
+  setIsLoggedIn(false);
+  // display message on homeelement letting user know account was created successfully
+  setMessageToDisplay(`${loggedInUser} you are now signed out`);
+  setDisplayAlert(true);
+  okToHideNavBar.current = false;
+  localStorage.removeItem("user");
+  // send back to home after form submitted
+  navigate("/");
+  return;
 }
 
-export default Logout
+export default Logout;
