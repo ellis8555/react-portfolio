646b0d3992955720c5825d558463782fa6ec40e6
ellis <lsp1@hotmail.com>
2023-05-10 12:24:45 -0600

    login.jsx displays login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
+import LoadingAnimation from "../../utils/components/LoadingAnimation";
 import BodyContainer from "../../utils/components/BodyContainer";
-
-function LoginContent() {
-  return <p className="h4 text-warning">Under construction</p>;
-}
+import LoginForm from "./login-form-sub-components/LoginForm";
 
 function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <BodyContainer title="Login">
-      <LoginContent />
-    </BodyContainer>
+    <>
+      <BodyContainer title="Login"></BodyContainer>
+      {isLoading && <LoadingAnimation />}
+      <div className=" md:flex md:justify-center">
+        <BodyContainer width="md:w-3/4 lg:w-1/2">
+          <LoginForm setIsLoading={setIsLoading} />
+        </BodyContainer>
+      </div>
+    </>
   );
 }
 
